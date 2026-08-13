64b00b3d634a36454be24382ba23df65710e1666
ayush saini <ayushsaini@ayush-MacBook-Air.local>
2026-08-13 18:19:26 +0530

    Update Express-Class

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require ('express');
+// const express = require ('express');
+// const app = express();
 
-const app = express();
+const packages = require("./tour");
 
-app.get(`/`,(req,res)=>{
-    res.send("hello world");
-})
+// app.get(`/`,(req,res)=>{
+//     res.send("hello world");
+// });
 
-app.listen(3000,()=>{
-    console.log("server is running on 3000");
-})
+app.get("/packages",(req,res)=>{
+    res.json(packages);
+});
+
+app.get("/packages/:id",(req,res)=>{
+    const packageId = parseInt(req.params.id);
+    const selectedPackage = packages.find(item => item.id ===
+    packageId);
+    res.json(selectedPackage);
+    });
+
+// app.listen(3000,()=>{
+//     console.log("server is running on 3000");
+// })
