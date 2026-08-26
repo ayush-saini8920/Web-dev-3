a3cf449b93b5bea0e1d529cc979316ab7570f071
Ayush Saini <2501010190@krmu.edu.in>
2026-08-26 12:12:09 +0530

    Updated work

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// const express = require ('express');
-// const app = express();
+const express = require ('express');
+const app = express();
 
 const packages = require("./tour");
 
-// app.get(`/`,(req,res)=>{
-//     res.send("hello world");
-// });
+app.get(`/`,(req,res)=>{
+    res.send("hello world");
+});
 
 app.get("/packages",(req,res)=>{
     res.json(packages);
@@ -18,6 +18,6 @@ app.get("/packages/:id",(req,res)=>{
     res.json(selectedPackage);
     });
 
-// app.listen(3000,()=>{
-//     console.log("server is running on 3000");
-// })
+app.listen(3000,()=>{
+    console.log("server is running on 3000");
+})
